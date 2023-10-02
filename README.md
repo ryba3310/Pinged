48479530ce168cb272815f73cd57927ca4028e2f
Ryba <michal8876@gmail.com>
2023-10-02 20:55:32 +0200

    Receiving functionality added

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Supplied data may be encoded into base64 with "-b" flag before transmission.
 
 It is developed for the sake of breviy and is inspired by Netcat.
 
+Root privileges or CAP_NET_RAW capabilities set are required for usage of SOCK_RAW from socket library to create socket and allow sending and recieving packets.
+
 # About
 
 Please keep in mind it's not developed in a malicious puropse therefore any IDS properly configured should detect the trafic and it is not suited for transmission of large payloads becouse of DoS danger.
@@ -29,3 +31,5 @@ Check data integrity and in case of error, request it again from client
 Create client mode if there is an argument which indicates IP destination which by default accepts STDIN as input or file spcifed by "-f" flag
 
 Listen for resend requests in client mode
+
+Check IPPROTO_ICMP protocol and SOCK_DGRAM type with sysctl 'net.ipv4.ping_group_range allowed' for rootless application
--- pinged.c
+++ pinged.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>         // exit() func
+#include <unistd.h>         // getoopt() library
+#include <sys/socket.h>     // socket library, domain arg macro(AF_INET for IPv4)
+#include <sys/types.h>      // socket library for the sake of potrability, Linux doesnt requiere this library
+#include <string.h>         // memset() func
+#include <errno.h>          // for in case of socket fail errno gets updated
+#include <netinet/ip.h>     // ip header structure and macros, netinet/in.h is also valid, INADDR_ANY = 0.0.0.0
+#include <netinet/ip_icmp.h>   // icmp header structure and macros
 
+
+#define MAX_ICMP_S 65536
+
+
+
+void listen_for_icmp();
 
 int main(int argc, char *argv[])
 {
 
     if (argc == 1)
     {
-        /* listen_for_icmp(); */
+        listen_for_icmp();
     }
     else
     {
@@ -32,10 +45,40 @@ int main(int argc, char *argv[])
                     printf("Unknown option %c\n", optopt);
                     break;
                 case ':':
-                    printf("Option -%c requires a path to file\n", optopt);
+                    fprintf("Option -%c requires a path to file\n", optopt);
                     break;
             }
         }
     }
     return 0;
+}
+
+
+void listen_for_icmp()
+{
+    int socket_fd;
+
+    if((socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
+    {
+        perror("Error getting socket number\n");
+        exit(1);
+    }
+
+    struct sockaddr_in sock_addr;
+    char buffer[MAX_ICMP_S];
+    int pkt_size;
+    socklen_t sock_addr_s;      // recvform expect socklen_t type
+    sock_addr_s = sizeof(sock_addr);
+
+    while(1)
+    {
+        if((pkt_size = recvfrom(socket_fd, buffer, MAX_ICMP_S, 0, &sock_addr, &sock_addr_s)) < 0)
+        {
+            perror("Error recvform() func\n");
+            exit(1);
+        }
+        printf("Got packet of length: %d\n", pkt_size);
+    }
+    // parse and print all metadata and then print data
+
 }
